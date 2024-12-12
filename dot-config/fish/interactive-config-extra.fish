fe3d2c285ba4a01a5bc6cb021d27547358858fd8
grig-iv <abstractgrig@gmail.com>
2024-12-12 19:50:04 +0000

    add abbrs

--- dot-config/fish/interactive-config-extra.fish
+++ dot-config/fish/interactive-config-extra.fish
@@ -1,5 +1,10 @@
 abbr -a stw 'stow --dotfiles --no-folding -t $HOME -d $HOME/.config -S dotfiles'
+
+abbr -a gc 'cd $HOME/.config'
 abbr -a gd 'cd $HOME/.config/dotfiles'
+abbr -a gn 'cd $HOME/.config/nvim'
+abbr -a gs 'cd $HOME/sources'
+abbr -a gx 'cd $NIXCONF'
 
 bind \cq 'exit'
 bind \cl 'clear; commandline -f repaint'
