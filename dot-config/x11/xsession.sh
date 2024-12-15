b39256624279fa571de0d91d5be567161c7c8cb9
grig-iv <abstractgrig@gmail.com>
2024-12-15 22:18:34 +0300

    create xsession

---
+++ dot-config/x11/xsession.sh
@@ -0,0 +1,19 @@
+if [ -f $HOME/.config/wallpaper.tif ]; then
+    feh --bg-fill $HOME/.config/wallpaper.jpg
+fi
+
+if [ "$DESKTOP_SESSION" == "none+dwm" ]; then
+    gost &
+    dwm &> $HOME/.local/share/dwm.log
+fi
+
+if [ "$DESKTOP_SESSION" == "none+mind-shift" ]; then
+    mind-shift-st &
+    mind-shift &> $HOME/.local/share/mind-shift.log
+
+    while [ -e $HOME/sources/mind-shift/mind-shift ]; do
+        mv -f $HOME/sources/mind-shift/mind-shift /tmp/mind-shift
+        /tmp/mind-shift &> $HOME/sources/mind-shift/mind-shift.log
+        mind-shift &> $HOME/.local/share/mind-shift.log
+    done
+fi
