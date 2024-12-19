220b0ebaea3a0d617944bfa9a45d3768ce52c5f9
grig-iv <abstractgrig@gmail.com>
2024-12-20 00:43:23 +0300

    fish update

--- dot-config/fish/config-extra.fish
+++ dot-config/fish/config-extra.fish
@@ -34,11 +34,10 @@ status is-interactive; and begin
     abbr -a gd 'cd $CONFIG/dotfiles'
     abbr -a gn 'cd $CONFIG/nvim'
     abbr -a gs 'cd $HOME/sources'
-    abbr -a gm 'cd $MIND'
     abbr -a gx 'cd $NIXCONF'
 
-    abbr -a s 'jump -d $HOME/sources/'
-    abbr -a m 'cd $MIND & $EDITOR context.md'
+    abbr -a jc 'jump -d $CONFIG'
+    abbr -a js 'jump -d $HOME/sources/'
 
     # verbosity and settings that you pretty much just always are going to want
     abbr -a cp 'cp -ivr'
@@ -62,6 +61,10 @@ status is-interactive; and begin
     # go
     abbr -a gmt 'go mod tidy'
     abbr -a gmi 'go mod init'
+
+    # notes
+    abbr -a jm 'cd $MIND & $EDITOR context.md'
+    abbr -a gm 'cd $MIND'
 
     # misc
     abbr -a stw 'stow --dotfiles --no-folding -t $HOME -d $CONFIG -S dotfiles'
