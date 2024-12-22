40ef38301fb6b4bd23e53839a2e21197fe92f4fc
grig-iv <abstractgrig@gmail.com>
2024-12-22 15:45:12 +0300

    fish fix colors

--- dot-config/fish/config-extra.fish
+++ dot-config/fish/config-extra.fish
@@ -30,7 +30,7 @@ setup_hm_session_vars
 
 status is-interactive; and begin
     direnv hook fish | source
-    source colors.fish
+    source "$CONFIG/fish/colors.fish"
 
     # shortcuts
     abbr -a gc 'cd $CONFIG'
