50b0d64d8cae56d438d899ab0903549b73f4984a
grig-iv <abstractgrig@gmail.com>
2024-12-22 17:04:09 +0300

    yazi add

---
+++ dot-config/yazi/init.lua
@@ -0,0 +1,21 @@
+require('session'):setup {
+    sync_yanked = true,
+}
+
+
+-- {{ show symlink in status bar
+
+function Status:name()
+    local h = cx.active.current.hovered
+    if h == nil then
+        return ui.Span('')
+    end
+
+    local linked = ''
+    if h.link_to ~= nil then
+        linked = ' -> ' .. tostring(h.link_to)
+    end
+    return ui.Span(' ' .. h.name .. linked)
+end
+
+-- }}
