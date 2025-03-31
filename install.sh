dd0e0b231fed00e32ef939f9b26ef4103c30d85c
grig-iv <abstractgrig@gmail.com>
2025-04-01 00:57:50 +0300

    install script update

--- install.sh
+++ install.sh
@@ -12,6 +12,7 @@ sed -i 's/"--noclear"/"--noclear --autologin grig"/' /etc/sv/agetty-tty1/conf
 
 # enable services
 ln -sf /etc/sv/dbus /var/service
+ln -sf /etc/sv/nix-daemon /var/service
 ln -sf /etc/sv/cupsd /var/service
 ln -sf /etc/sv/avahi-daemon /var/service
 ln -sf /etc/sv/ipp-usb /var/service
