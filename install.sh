02f73d9fcd816fcab769cede28440012ac4e2cd2
grig-iv <abstractgrig@gmail.com>
2025-03-31 23:40:13 +0300

    xinitrc update

--- install.sh
+++ install.sh
@@ -4,8 +4,17 @@ xbps-install -Su
 xbps-install -Su void-repo-nonfree just
 just install-pkgs
 
+# printing group
 usermod -a -G lpadmin grig
 
+# enable autologin
+sed -i 's/"--noclear"/"--noclear --autologin grig"/' /etc/sv/agetty-tty1/conf
+
+# enable services
+ln -sf /etc/sv/dbus /var/service
 ln -sf /etc/sv/cupsd /var/service
 ln -sf /etc/sv/avahi-daemon /var/service
 ln -sf /etc/sv/ipp-usb /var/service
+
+# stow
+just -f ./stow stow-root
