baa349d2fc4ad2afaa0efd1e78a396259c8e7d25
grig-iv <abstractgrig@gmail.com>
2025-04-11 01:17:01 +0300

    fish update

--- stow/home/dot-config/fish/config.fish
+++ stow/home/dot-config/fish/config.fish
@@ -1,34 +1,3 @@
-function setup_hm_session_vars
-    if [ -n "$__SESS_VARS_SOURCED" ]
-        return
-    end
-    set -gx __SESS_VARS_SOURCED 1
-
-    set -gx PATH "$PATH"(test -n "$PATH" && echo ':' || echo)"$GOBIN"':'"$HOME"'/.local/bin'
-
-    set -gx XDG_CACHE_HOME '/home/grig/.cache'
-    set -gx XDG_CONFIG_HOME '/home/grig/.config'
-    set -gx XDG_DATA_HOME '/home/grig/.local/share'
-    set -gx XDG_STATE_HOME '/home/grig/.local/state'
-
-    set -gx BROWSER firefox
-    set -gx TERMINAL wezterm
-    set -gx EDITOR nvim
-
-    set -gx CONFIG "$HOME/.config"
-    set -gx MIND "$HOME/mind"
-    set -gx NVIMCONF "$CONFIG/nvim"
-    set -gx NIXCONF /etc/nixos
-    set -gx SVDIR "$HOME/.local/service"
-
-    set -gx SKIM_DEFAULT_OPTIONS '--color=fg:#cdd6f4,bg:empty,matched:#89dceb,matched_bg:#1e1e2e,current:#fab387,current_bg:#313244,current_match:#1e1e2e,current_match_bg:#89dceb,spinner:#a6e3a1,info:#bac2de,prompt:#cdd6f4,cursor:#fab387,selected:#eba0ac,header:#94e2d5,border:#6c7086 --bind \'ctrl-q:abort\''
-
-    set -gx XCURSOR_PATH "$XCURSOR_PATH"(test -n "$XCURSOR_PATH" && echo ':' || echo)'/etc/profiles/per-user/grig/share/icons'
-    set -gx XCURSOR_SIZE 16
-    set -gx XCURSOR_THEME Bibata-Modern-Ice
-end
-setup_hm_session_vars
-
 status is-interactive; and begin
     if type -q direnv
         direnv hook fish | source
@@ -44,6 +13,7 @@ status is-interactive; and begin
     bind \cr 'run; commandline -f repaint'
 
     # shortcuts
+    abbr -a gg 'cd "$(cat $HOME/.config/bookmarks/dirs | sk | sed \'s/\~/\/home\/grig/g\')"'
     abbr -a gh 'cd $HOME'
     abbr -a gc 'cd $CONFIG'
     abbr -a gd 'cd $CONFIG/dotfiles'
@@ -57,7 +27,7 @@ status is-interactive; and begin
 
     abbr -a c 'jump -d $CONFIG && yacd'
     abbr -a s 'jump -d $HOME/sources/ && yacd'
-    abbr -a d 'jump -dr $CONFIG/dotfiles && yacd'
+    abbr -a d 'jump -r $CONFIG/dotfiles && yacd'
     abbr -a t 'jump -d $HOME/media/T7/torrents'
 
     abbr -a cp 'cp -ivr'
