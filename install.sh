2ce0915b2b0aba0a59392f901007622537624920
grig-iv <abstractgrig@gmail.com>
2025-04-11 01:27:37 +0300

    xkb add my ru layout variant

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env dash
+#!/usr/bin/env sh
 
 xbps-install -Su
 xbps-install -Su void-repo-nonfree just
@@ -17,8 +17,14 @@ ln -sf /etc/sv/cupsd /var/service
 ln -sf /etc/sv/avahi-daemon /var/service
 ln -sf /etc/sv/ipp-usb /var/service
 
-# stow
-just -f ./stow stow-root
-
 # user media
 ln -sf "/run/media/grig" "$HOME/media"
+
+# add my layout
+xkbFile="/usr/share/X11/xkb/symbols/ru"
+if [ -z "$(grep 'xkb_symbols "grig" {'  $xkbFile)" ]; then 
+    cat "./xkb/ru-grig.xkb" >> $xkbFile
+fi
+
+# stow
+just -f ./stow stow-root
