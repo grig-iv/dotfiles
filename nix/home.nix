73afc5080d722cdb5fb3e09c6499bd9f4b13e753
grig-iv <abstractgrig@gmail.com>
2025-06-14 01:22:37 +0300

    nix add pkgs

--- nix/home.nix
+++ nix/home.nix
@@ -1,8 +1,43 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
+    age
+    fd
+    skim
+    jq
+    tree
+    bottom
+    tldr
+    neovim
+    helix
+    tmux
+    tmuxp
+    yazi
+    glow
+    ripgrep
+    just
+    stow
+    lazygit
+
+    shadowsocks-rust
+    syncthing
+
+    curl
+    wget
+
+    # dev
     nil
     alejandra
     marksman
+
+    go
+
+    # desktop-shell
+    xkb-switch
+    unclutter-xfixes
+
+    # desktop
+    reaper
+    vital
   ];
 
   programs.home-manager.enable = true;
