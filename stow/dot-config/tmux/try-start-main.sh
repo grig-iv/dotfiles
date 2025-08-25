871d08536361a64a0345e53ae6e71327afa06f6e
grig-iv <abstractgrig@gmail.com>
2025-08-26 02:00:17 +0300

    update

--- stow/dot-config/tmux/try-start-main.sh
+++ stow/dot-config/tmux/try-start-main.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Compact function for Ghostty init: connect to "main" tmux if unattached, else start fish
-#
+source "$HOME/.bashrc"
+
 if tmux has-session -t main 2>/dev/null && [ "$(tmux list-sessions -F "#{session_name} #{session_attached}" | awk '/^main / {print $2}')" -eq 0 ]; then
     tmux attach-session -t main
 elif ! tmux has-session -t main 2>/dev/null; then
