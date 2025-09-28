0859ca13221613b2559bb45a98cd3fe74ebcc30a
grig-iv <abstractgrig@gmail.com>
2025-09-29 01:12:44 +0300

    update fish.config

--- stow/dot-config/fish/functions/run.fish
+++ stow/dot-config/fish/functions/run.fish
@@ -2,5 +2,8 @@ function run
     if test -e "./main.go"
         commandline -r "go run ."
         commandline -f execute
+    else if  test -e "./bun.lock"
+        commandline -r "bun run dev"
+        commandline -f execute
     end
 end
