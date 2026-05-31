84dd0c6044eb775bb9f9c173bc96198a4a15037f
grig-iv <abstractgrig@gmail.com>
2026-05-31 18:56:56 +0300

    add fastfetch

--- stow/dot-config/tmux/try-start-main.sh
+++ stow/dot-config/tmux/try-start-main.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
-# Compact function for Ghostty init: connect to "main" tmux if unattached, else start fish
 source "$HOME/.bashrc"
 
 if tmux has-session -t main 2>/dev/null && [ "$(tmux list-sessions -F "#{session_name} #{session_attached}" | awk '/^main / {print $2}')" -eq 0 ]; then
     tmux attach-session -t main
 elif ! tmux has-session -t main 2>/dev/null; then
-    tmux new-session -s main
+    # create session
+    tmux new-session -d -s main
+
+    # wait until fish panel is started
+    until tmux list-panes -t main -F "#{pane_current_command}" | grep -q fish; do
+        sleep 0.1
+    done
+
+    # run fastfetch
+    tmux send-keys -t main "fastfetch" Enter
+
+    # attach to session
+    tmux attach-session -t main
 else
     exec fish
 fi
